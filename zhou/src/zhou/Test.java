c9aec5bb8606f57938c620f49bd945b8be648396
smilezhou <smilezzhou@163.com>
2015-05-20 22:58:59 +0800

    myeclipse commit to local

--- zhou/src/zhou/Test.java
+++ zhou/src/zhou/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("develop !");
 		System.out.println("Hello word");
+		System.out.println("eclipse commit");
 	}
-
 }
